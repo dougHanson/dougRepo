0dced14d643525fa3b8dd256de6834cf32c559bf
Doug83 <doug83@iinet.net.au>
2017-01-18 14:56:10 +0800

    add missing partial

---
+++ inc/_nav.php
@@ -0,0 +1,46 @@
+  <!-- frame borders -->
+  <div class="frame frame__top"></div>
+  <div class="frame frame__bottom"></div>
+  <div class="frame frame__left"></div>
+  <div class="frame frame__right"></div>
+  <!-- end frame borders --> 
+      
+  <!-- left hand nav include -->
+  <aside class="vertical-menu">
+    <div class="vertical-menu__logo"><a href="/"><img src="img/namelogo.png" /></a></div>
+    <nav>
+      <ul>
+        <li class="nav-home"><a href="#">					
+					<i class="icon">
+						<?php //echo file_get_contents("svg/home.svg"); ?>
+					</i><span>Home</span></a>
+				</li>
+				
+				<li class="nav-works"><a href="#">
+					<i class="icon">
+						<?php // echo file_get_contents("svg/briefcase.svg"); ?>
+					</i><span>Selected works</span></a>
+				</li>
+				
+        <li class="nav-about"><a href="#">					
+					<i class="icon">
+						<?php //echo file_get_contents("svg/about.svg"); ?>	
+					</i><span>About me</span></a>
+				</li>
+									
+        <li class="nav-contact"><a href="#">
+					<i class="icon">
+						<?php //echo file_get_contents("svg/contact.svg"); ?>
+					</i><span>Contact</span></a>
+				</li>       
+				
+				<!--<li class="nav-contact"><a href="#"><i class="icon svg-mail svg-pen-dims"></i><span>Contact</span></a></li>-->				
+      </ul>
+			
+			<br><br>
+			<small style="font-size: .85em; font-weight: 100; text-transform: none;">My social links</small>
+			
+			
+    </nav>
+  </aside>
+  <!-- end left hand nav include --> 
