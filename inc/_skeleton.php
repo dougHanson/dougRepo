fc3d77e872bc3cc2c2a22e9364a9d244a5ba3acd
Doug <doug83@iinet.net.au>
2018-08-02 08:50:46 +0800

    clean up svgs

--- inc/_skeleton.php
+++ inc/_skeleton.php
@@ -1,16 +1,16 @@
-	<div class="skeleton">
-		<div class="skeleton__aside">
-			<div class="skeleton__logo"></div>
-			
-			<div class="skeleton__nav"></div>
-			<div class="skeleton__nav"></div>
-			<div class="skeleton__nav"></div>
-			<div class="skeleton__nav"></div>
-		</div>
-		<div class="skeleton__homepage">
-			<div class="skeleton__bar1"></div>
-			<div class="skeleton__bar2"></div>
-			<p>Site loading...</p>
-			<img src="img/loading.gif" />
-		</div>
+<div class="skeleton">
+	<div class="skeleton__aside">
+		<div class="skeleton__logo"></div>
+
+		<div class="skeleton__nav"></div>
+		<div class="skeleton__nav"></div>
+		<div class="skeleton__nav"></div>
+		<div class="skeleton__nav"></div>
 	</div>
+	<div class="skeleton__homepage">
+		<div class="skeleton__bar1"></div>
+		<div class="skeleton__bar2"></div>
+		<p>Site loading...</p>
+		<img src="img/loading.gif" />
+	</div>
+</div>
