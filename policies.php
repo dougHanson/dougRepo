bebc62889e1e212788a8131b61c3f952862e9444
Doug Hanson <doug.hanson@education.wa.edu.au>
2020-08-17 13:24:57 +0800

    update policies content and add screenshots

--- policies.php
+++ policies.php
@@ -56,50 +56,94 @@
             <div class="col-lg-9">
 
             <p class="h3 subheading padding-0 margin-0">At a glance</p>
-              <p>The existing Department of Education Policies website was extremely outdated and hard to use. My goal was to improve the user experience for both end users and authors, whilst designing a visually appealing website.</p>
+              <p>The existing Department of Education Policies website was extremely outdated and hard to use. My goal was to improve the <strong>user experience</strong> for both end users and <strong>web content authors</strong>, while designing a more visually appealing website.</p>
               <!-- <div class="item margin-bottom-20">
                 <img src="img/loading.gif" data-src="img/policies-old-site.png" alt="Icon design" class="lazy-load img-center img-frame">
               </div>
               <p class="margin-bottom-20">&nbsp;</p> -->
 
-              <p class="h3 subheading padding-0 margin-0">Designs</p>
-              <p>I aligned the branding with the Department of Education website and simplified the user journey by offering two main options on the homepage. Users can now choose to search for documents by category or by content type, which is important given the large volume of content.</p>
-              <div class="owl-carousel owl-theme">
-                <div class="item margin-bottom-40">
-                  <a href="img/policies-design-1.png" data-lightbox="code" data-title="Policies UI design">
-                    <img src="img/loading.gif" data-src="img/policies-design-1.png" alt="Policies homepage" class="lazy-load img-frame img-center">
-                  </a>
-                </div>
-                <div class="item margin-bottom-40">
-                  <a href="img/policies-design-2.png" data-lightbox="code" data-title="Policies UI design">
-                    <img src="img/loading.gif" data-src="img/policies-design-2.png" alt="Policies homepage" class="lazy-load img-frame img-center">
-                  </a>
-                </div>
-              </div>
-              <p class="margin-bottom-20">&nbsp;</p>
+              <p class="h3 subheading padding-0 margin-0">Web designs</p>
+              <p>I designed simple yet corporate templates which aligned with the Department of Education's <strong>website branding</strong>. I also <strong>improved the UX by simplifying the user journey</strong> through offering two main options on the homepage. Users can now choose to search for documents by category or by content type, which is important given the large volume of content.</p>
+              <p>I also created a new cohesive <strong>SVG icon set using Adobe Illustrator</strong>, for easy identification between different yet similar content types.</p>
 
-              <p>For easy identification between different yet similar content types, I created a cohesive SVG icon set for easy visual recognition.</p>
               <div class="item margin-bottom-20">
-                <img src="img/loading.gif" data-src="img/policies-icons.png" alt="Icon design" class="lazy-load img-center full-width">
+                <a href="img/policies-design-2.png" data-lightbox="code" data-title="Policies UI design">
+                  <img src="img/loading.gif" data-src="img/policies-design-2.png" alt="Policies homepage" class="lazy-load img-shadow img-center">
+                </a>
               </div>
               <p class="margin-bottom-20">&nbsp;</p>
 
 
               <p class="h3 subheading padding-0 margin-0">CSS over JS</p>
-              <p>By designing with progressive enhancement in mind the majority of the website can be used with JavaScript disabled. By focusing on developing semantic HTML without the use of JS has also made the site naturally accessible from the outset.</p>
-              <p>The mobile menu was developed by styling checkboxes.</p>
-              <p>Accordions were developed using checkboxes, adding CSS transitions to animate the expand and collapse.</p>
+              <p>By designing with <strong>progressive enhancement</strong> in mind, the majority of the website has full functionality with JavaScript disabled. By focusing on developing <strong>semantic HTML with minimal inclusion of JavaScript</strong> has made the site naturally <strong>keyboard navigable and screen reader accessible</strong> from the outset.</p>
+
+              <p>The <strong>responsive, off-canvas mobile menu</strong> was developed by styling checkboxes.</p>
+              <div class="owl-carousel owl-theme">
+              <div class="item margin-bottom-40">
+                  <a href="img/policies-mobile-menu.png" data-lightbox="CSS only mobile menu" data-title="Policies responsive mobile menu">
+                    <img src="img/loading.gif" data-src="img/policies-mobile-menu.png" alt="responsive mobile menu" class="lazy-load img-shadow img-center">
+                  </a>
+                </div>
+                <div class="item margin-bottom-40">
+                  <a href="img/policies-mobile-menu-code.png" data-lightbox="CSS only mobile menu" data-title="Policies responsive mobile menu">
+                    <img src="img/loading.gif" data-src="img/policies-mobile-menu-code.png" alt="responsive mobile menu" class="lazy-load img-frame img-center">
+                  </a>
+                </div>
+              </div>
+              <p class="margin-bottom-20">&nbsp;</p>
+
+              <p>Accordions were developed using checkboxes and adding <strong>CSS transitions</strong> to animate the expand and collapse.</p>
+              <div class="owl-carousel owl-theme">
+              <div class="item margin-bottom-40">
+                  <a href="img/policies-accordions.png" data-lightbox="CSS only accordions" data-title="Policies CSS accordions">
+                    <img src="img/loading.gif" data-src="img/policies-accordions.png" alt="CSS accordions" class="lazy-load img-shadow img-center">
+                  </a>
+                </div>
+                <div class="item margin-bottom-40">
+                  <a href="img/policies-accordions-code.png" data-lightbox="CSS only accordions" data-title="Policies CSS accordions">
+                    <img src="img/loading.gif" data-src="img/policies-accordions-code.png" alt="CSS accordions" class="lazy-load img-frame img-center">
+                  </a>
+                </div>
+              </div>
+              <p class="margin-bottom-20">&nbsp;</p>
+
               <p>Tabs were developed by styling radio buttons, which show and hide adjacent content when checked.</p>
+              <div class="owl-carousel owl-theme">
+              <div class="item margin-bottom-40">
+                  <a href="img/policies-tabs.png" data-lightbox="CSS only tabs" data-title="Policies CSS tabs">
+                    <img src="img/loading.gif" data-src="img/policies-tabs.png" alt="CSS tabs" class="lazy-load img-shadow img-center">
+                  </a>
+                </div>
+                <div class="item margin-bottom-40">
+                  <a href="img/policies-tabs-code.png" data-lightbox="CSS only tabs" data-title="Policies CSS tabs">
+                    <img src="img/loading.gif" data-src="img/policies-tabs-code.png" alt="CSS tabs" class="lazy-load img-frame img-center">
+                  </a>
+                </div>
+              </div>
+              <p class="margin-bottom-20">&nbsp;</p>
 
 
               <p class="h3 subheading padding-0 margin-0">Complex freemarker templates</p>
-              <p>Create structures to enable authors easy content creation process</p>
-              <p>Freemarker to populate and style content as it was created</p>
-              <p>Browse page uses JPList plugin, degrades gracefully.</p>
+              <p>Freemarker templates were developed to save the client time categorising content, making the content authoring experience as simple as possible by grouping content of the same content type server side before being displayed client side.</p>
+              <p>Because many of the pages shared similar functionality, <strong>Freemarker macros</strong> were created to reuse and reduce repetition of code.</p>
+              <p>To allow end users to filter all content across the site I implemented JPList, <strong>a jQuery plugin</strong>, which <strong>degrades gracefully</strong> on older browsers or for users with JS disabled.</p>
+              <div class="owl-carousel owl-theme">
+              <div class="item margin-bottom-40">
+                  <a href="img/policies-freemarker-template.png" data-lightbox="Freemarker template" data-title="Policies Freemarker template">
+                    <img src="img/loading.gif" data-src="img/policies-freemarker-template.png" alt="Freemarker template" class="lazy-load img-frame img-center">
+                  </a>
+                </div>
+                <div class="item margin-bottom-40">
+                  <a href="img/policies-freemarker-browse.png" data-lightbox="Freemarker template" data-title="Policies Freemarker template">
+                    <img src="img/loading.gif" data-src="img/policies-freemarker-browse.png" alt="Freemarker template" class="lazy-load img-frame img-center">
+                  </a>
+                </div>
+              </div>
+              <p class="margin-bottom-20">&nbsp;</p>              
 
 
-              <p class="h3 subheading padding-0 margin-0">The result</p>
-              <p>The result was a <strong>fully responsive, accessible, visually appealing</strong> job lisiting portal, with an <strong>improved user experience</strong> for end users and content authors alike.</p>
+              <p class="h3 subheading padding-0 margin-0">Additional screenshots</p>
+              <p>The result was a <strong>fully responsive, accessible and visually appealing</strong> job lisiting portal, with an <strong>improved user experience</strong> for end users and web content authors alike.</p>
 
             </div>
             <!-- end page specific content -->
