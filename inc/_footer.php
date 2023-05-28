8befa0fe2369a67d9a41a49fd2946aed086e25f5
Doug Hanson <doug83@iinet.net.au>
2023-05-28 15:34:17 +0800

    update google analytics to latest version GA4 update npm packages

--- inc/_footer.php
+++ inc/_footer.php
@@ -82,22 +82,16 @@
 
 <!-- Google Analytics -->
 <?php if ($_SERVER['SERVER_NAME'] != 'localhost') { ?>
-<script>
-  (function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-      (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-  ga('create', 'UA-81920963-1', 'auto');
-  ga('send', 'pageview');
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GV8YRYJWCH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GV8YRYJWCH');
 </script>
+
 <?php } ?>
 <!-- end Google Analytics -->
 
